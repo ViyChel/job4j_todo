23a688c114dc81a82b85de3576d827f80dc764a9
Vitaly Yagufarov <for.viy@gmail.com>
2020-12-22 00:34:43 +0500

    Add index.html, db.properties, log4j.properties

---
+++ src/main/java/ru/job4j/todo/controller/ItemServlet.java
@@ -0,0 +1,26 @@
+package ru.job4j.todo.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Class ServletItem.
+ *
+ * @author Vitaly Yagufarov (for.viy@gmail.com)
+ * @version 1.0
+ * @since 22.12.2020
+ */
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
